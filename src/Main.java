82153b663bdde3eb0c5aec0e6322f248d64adc4f
home <timurfedorov042@gmail.com>
2025-02-24 09:04:58 +0300

    SH-HW-6

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,50 @@ public class Main {
         byte clientOS = 1;
         int clientDeviceYear = 2015;
         int year = 2021;
-        int deliveryDistance = 95;
-        byte days = 1;
-        byte monthNumber = 12;
 
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
             if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            } else {
+                if (clientDeviceYear >= 2015) {
+                    System.out.println("Установите полную версию приложения для iOS по ссылке.");
+                }
             }
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке.");
             if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+            } else {
+                if (clientDeviceYear >= 2015) {
+                    System.out.println("Установите полную версию приложения для Android по ссылке.");
+                }
             }
         } else {
-            System.out.println("Неизвестная операционная система."); // Обработка некорректного clientOS
+            System.out.println("Неизвестная операционная система.");
         }
 
         if (year < 1584) {
             System.out.println(year + " год должен быть больше 1584 для определения високосности.");
         } else {
-            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-                System.out.println(year + " год является високосным.");
+            if (year % 4 == 0) {
+                if (year % 100 != 0) {
+                    System.out.println(year + " год является високосным.");
+                } else {
+                    if (year % 400 == 0) {
+                        System.out.println(year + " год является високосным.");
+                    } else {
+                        System.out.println(year + " год не является високосным.");
+                    }
+                }
             } else {
                 System.out.println(year + " год не является високосным.");
             }
         }
 
+
+        int deliveryDistance = 95;
+        String days = "";
         if (deliveryDistance < 0) {
             System.out.println("Некорректное значение расстояния.");
         } else if (deliveryDistance <= 20) {
@@ -43,19 +59,28 @@ public class Main {
             System.out.println("Доставки нет.");
         }
 
+        int monthNumber = 12;
         if (monthNumber >= 1 && monthNumber <= 12) {
             String season = "";
             switch (monthNumber) {
-                case 1, 2, 12:
+                case 1:
+                case 2:
+                case 12:
                     season = "Зима";
                     break;
-                case 3, 4, 5:
+                case 3:
+                case 4:
+                case 5:
                     season = "Весна";
                     break;
-                case 6, 7, 8:
+                case 6:
+                case 7:
+                case 8:
                     season = "Лето";
                     break;
-                case 9, 10, 11:
+                case 9:
+                case 10:
+                case 11:
                     season = "Осень";
                     break;
             }
